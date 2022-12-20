6cd03963ded40949f10186a743fa1d8e8376ec5b
xkaska02 <xkaska02@stud.fit.vutbr.cz>
2022-12-20 19:12:33 +0100

    day 3 completed

--- day3.c
+++ day3.c
@@ -20,7 +20,7 @@ int secondHalfString(char original[], char secondHalf[], int size){
 }
 
 int main(){
-    FILE * input = fopen("day3_input.txt", "r");
+    FILE * input = fopen("day3_simple_input.txt", "r");
     if(input == NULL){
         fprintf(stderr, "malloc error");
         return 1;
@@ -29,14 +29,9 @@ int main(){
     char line[50];
     char halfLine[25];
     int sum = 0;
-    // fgets(line, 50, input);
-    // secondHalfString(line, halfLine, strlen(line));
-    // printf("%s", halfLine);
     // naive solution
-    while(fgets(line, 50, input)){
-        //printf("%c", line[0]);
+    /* while(fgets(line, 50, input)){
         secondHalfString(line,halfLine,strlen(line));
-        //printf("%s\n", halfLine);
         for(int i = 0; i<strlen(line)/2; i++){
             if(strchr(halfLine, line[i]) != NULL){
                 if(islower(line[i])){
@@ -47,9 +42,30 @@ int main(){
                     break; // it is given in the task that only 1 item per line is in both halves
                 }
             }
-            //break; 
         }         
+    } */
+
+    // part two
+    char line2[50];
+    char line3[50];
+    while(fgets(line, 50, input)){
+        fgets(line2, 50, input);
+        fgets(line3, 50, input);
+        for(int i = 0; i < strlen(line); i++){
+            if(strchr(line2, line[i])){
+                if(strchr(line3, line[i])){
+                    if(islower(line[i])){
+                    sum += line[i] - 'a' + 1;
+                    break; // it is given in the task that only 1 item per line is in both halves
+                }else if(isupper(line[i])){
+                    sum += line[i] - 'A' + 27;
+                    break; // it is given in the task that only 1 item per line is in both halves
+                }
+                }
+            }
+        }
     }
+
     printf("%d\n", sum);
     return 0;
 }
