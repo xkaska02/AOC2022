55bba43338f7e7630ffe33f27e41c344ad12732b
xkaska02 <xkaska02@stud.fit.vutbr.cz>
2022-12-20 12:59:56 +0100

    task 2 completed too lazy to refactor so i just put first half in comment

--- day2.c
+++ day2.c
@@ -38,6 +38,41 @@ int RPS(char myChoice, char oponentChoice){
     }
 }
 
+int RPS_choose_option(char oponentChoice, char outcome){
+    switch (oponentChoice)
+    {
+    case 'A':
+        if(outcome == 'X'){
+            return 3; //0 for losing + 3 for choosing scissors
+        }else if(outcome == 'Y'){
+            return 4; // 3 for drawing + 1 for rock
+        }else if(outcome == 'Z'){
+            return 8; // 6 for winning + 2 for paper
+        }
+        break;
+    case 'B':
+        if(outcome == 'X'){
+            return 1; //0 for losing + 1 for rock
+        }else if(outcome == 'Y'){
+            return 5; // 3 for drawing + 2 for paper
+        }else if(outcome == 'Z'){
+            return 9; // 6 for winning + 3 for scissors
+        }
+        break;
+    case 'C':
+        if(outcome == 'X'){
+            return 2; //0 for losing + 2 for paper
+        }else if(outcome == 'Y'){
+            return 6; // 3 for drawing + 3 for scissors
+        }else if(outcome == 'Z'){
+            return 7; // 6 for winning + 1 for rock
+        }
+        break;
+    default:
+        break;
+    }
+}
+
 int main(){
     FILE * input = fopen("day2_input.txt", "r");
     if(input == NULL){
@@ -47,7 +82,7 @@ int main(){
     char line[5];
     int score = 0;
     while(fgets(line, 5, input)){
-        // switch for choices i should play
+     /*    // switch for choices i should play
         switch (line[2])
         {
         case 'X':
@@ -67,7 +102,8 @@ int main(){
         default:
             fprintf(stderr, "wrong input");
             break;
-        }
+        } */
+        score += RPS_choose_option(line[0], line[2]);
     }
     printf("score: %d\n", score);
     return 0;
